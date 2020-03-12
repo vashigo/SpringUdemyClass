2c84fba7a7f666a86ea4dfde7dfb5b20f7169a70
Andres Vasque <andresdavid.vasquez@gmail.com>
2020-03-12 16:25:46 -0500

    32. step 23 END

--- src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsXMLContextApplication.java
+++ src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsXMLContextApplication.java
@@ -1,23 +1,26 @@
 package com.in28minutes.spring.basics.springin5steps;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.in28minutes.spring.basics.springin5steps.xml.XmlPersonDAO;
 
-@Configuration
-@ComponentScan
 public class SpringIn5StepsXMLContextApplication {
+
+	private static Logger LOGGER = LoggerFactory.getLogger(SpringIn5StepsScopeApplication.class);
 
 	public static void main(String[] args) {
 
 		try (ClassPathXmlApplicationContext applicationContext = new ClassPathXmlApplicationContext(
 				"applicationContext.xml")) {
 
+			LOGGER.info("Beans Loaded -> {}", (Object) applicationContext.getBeanDefinitionNames());
+			// [xmlJdbcConnection, xmlPersonDAO]
+
 			XmlPersonDAO personDao = applicationContext.getBean(XmlPersonDAO.class);
-			System.out.println(personDao);
-			System.out.println(personDao.getXmlJdbcConnection());
+
+			LOGGER.info("{} {}", personDao, personDao.getXmlJdbcConnection());
 		}
 	}
 }
